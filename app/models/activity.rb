ab61bd3125400fda2c84c5807a1b1287369a1064
Vincent Katz <vinz.k@live.fr>
2020-05-25 16:49:52 +0300

    made a few edits

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -6,10 +6,10 @@ class Activity < ApplicationRecord
   validates :title, presence: true
   validates :start_time, presence: true
   validates :end_time, presence: true
-  validates :player_limit, presence: true
+  validates :player_limit, presence: true, numericality: true
   validates :location, presence: true
   validates :skill_level, inclusion: { in: %w[Beginner Intermediate Pro] }
-  validates :description, presence: true, length: { minimum: 20 }
+  validates :description, presence: true
   validates :sport, presence: true
   validates :user, presence: true
 end
