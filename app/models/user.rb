522a55fd3016932d2ae50e26c4cf4aeec5a333b7
Schneur Kamensky <szkamens@gmail.com>
2020-05-25 16:02:36 +0300

    .

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :players
+  has_many :activites, through: :players
+  has_many :sports, through: :sports_users
+
   validates :username, presence: true, uniqueness: true, length: { minimum: 4}, 
   validates :gender, presence: true, inclusion { in: %w(Man Lady)}
   validates :description, presence: true, length { minimum: 20}
-
-  has_many :players
-  has_many :sports_users
-  has_many :activites, through: :players
 end
