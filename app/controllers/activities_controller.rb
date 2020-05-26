bbb3395319fde3e1c6d612466f0224fc46550d8c
Naftali Ajnwojner <naftali.ajnwojner@gmail.com>
2020-05-26 17:55:21 +0300

    if conditions for backend

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,10 @@
 class ActivitiesController < ApplicationController
+
+
+
+def show
+  @activity = Activity.find(params[:id])
+end
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts "Seeding Users"
+user1 = User.new(username: "Boby", email: "bob@bob.com", password:"123456", password_confirmation: "123456", description: "Good at tennis", gender:"Male")
+user1.save
+puts "Seeding done congrats, #{User.count} users created !"
+
+activity1 = Activity.new(title: "5 a side", start_time: "8pm", end_time:"10pm", player_limit: "10", location: "Hayarkon Park, Tlv", skill_level: "Beginner", description:" Become the new Messi", sport_id: 1, user_id: 1)
+activity1.save
+puts "Seeding done congrats, #{Activity.count} activities created !"
