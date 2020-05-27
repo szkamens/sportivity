628b863fc73ec9b054c889c3282b5a24e18feb6e
Vincent Katz <vinz.k@live.fr>
2020-05-27 09:38:53 +0300

    worked on new activity

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,22 @@
 class ActivitiesController < ApplicationController
+
+  def new
+    @activity = Activity.new
+  end
+
+  def create
+    @activity = Activity.new(activities_params)
+    @activity.user = current_user
+  end
+
+  private
+
+  def activities_params
+    params.require(:activity).permit(:title, :start_time, :end_time, :player_limit, :location, :skill_level, :description)
+  end
+
+  def set_activity
+    @activity = Activity.find(params[:id])
+  end
+
 end
