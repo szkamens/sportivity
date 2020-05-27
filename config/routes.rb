632ee43237325483b3a9e761d6ddd401befbd85b
ariel garbarsky <arig91@gmail.com>
2020-05-27 17:31:10 +0300

    not finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  
+
+  get 'dashboard', to: 'pages#dashboard', as: :dashboard
+
   resources :activities, only: %i[index new create show]
-  
+
 end
