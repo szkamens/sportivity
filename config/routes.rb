adcf7804fa5d2ad6eaa1e4e893954355e7fc46fe
Vincent Katz <vinz.k@live.fr>
2020-05-27 13:25:13 +0300

    connected show page and create page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :activities, only: [:new, :create]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/activities/:id', to: 'activities#show'
+  resources :activities, only: [:new, :create, :show]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,6 +18,6 @@ activity1 = Activity.new(title: "5 a side", start_time: "8pm", end_time:"10pm", 
 activity1.save
 puts "Seeding done congrats, #{Activity.count} activities created !"
 
-sport1 = Sport.new (name: "Football")
+sport1 = Sport.new(name: "Football")
 sport1.save
 
