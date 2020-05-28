6ab81c1f6535c5f7b21d0b0a95cbe32c335e3327
ariel garbarsky <arig91@gmail.com>
2020-05-28 12:48:04 +0300

    dashboard not done

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-<<<<<<< HEAD
 
   get 'dashboard', to: 'pages#dashboard', as: :dashboard
 
   resources :activities, only: %i[index new create show]
-=======
-  
-  resources :activities, only: [:index, :new, :create, :show, :destroy]
->>>>>>> 3c6c770230a9992ca6d5d8b60915d6fa4f5fee07
 
+  resources :activities, only: [:index, :new, :create, :show, :destroy]
 end
