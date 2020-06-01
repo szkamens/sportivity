d8ab5032103885b82c1ed87f57d6e6c3fc20ef7e
Schneur Kamensky <szkamens@gmail.com>
2020-06-01 15:10:53 +0300

    not working yet

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show]
+  before_action :set_user, only: %i[show edit update]
 
   def show
   end
@@ -8,10 +8,23 @@ class UsersController < ApplicationController
     @user = current_user
     render :show
   end
-  
+
+  def edit
+  end
+
+  def update
+    @user.sport = []
+    @user.update(user_params)
+    redirect_to me_path(@user)
+  end
+
   private
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:description, :skill_level, :sport_id)
   end
 end
